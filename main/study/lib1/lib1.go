3ad760b9846a9fcb58c16beeb80393f757f50c7f
Xiongzhixin <706316494@qq.com>
2025-08-11 14:18:43 +0800

    by XZX

---
+++ main/study/lib1/lib1.go
@@ -0,0 +1,15 @@
+package lib1
+
+import (
+	"fmt"
+)
+
+func Lib1Test() {
+	fmt.Println("lib1Test() ...")
+}
+
+
+func init() {
+	fmt.Println("lib1 init() ...")
+
+}
---
+++ main/study/test3.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	_ "go-study/main/study/lib1"
+	// mylib2 "go-study/main/study/lib2"
+	. "go-study/main/study/lib2"
+)
+
+func main() {
+	// lib1.Lib1Test()
+	// lib2.Lib2Test()
+	// mylib2.Lib2Test()
+	Lib2Test()
+}
