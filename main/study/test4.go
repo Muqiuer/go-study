a5369bdc909974e39c6b099e11cafec06b1c5530
Xiongzhixin <706316494@qq.com>
2025-08-17 21:20:44 +0800

    by XZX

---
+++ main/study/test4.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func swap(pa *int, pb *int) {
+	var temp int
+	temp = *pa
+	*pa = *pb
+	*pb = temp
+}
+
+func main() {
+	var a int = 10
+	var b int = 20
+	swap(&a, &b)
+	fmt.Println("a = ", a, "b = ", b)
+}
